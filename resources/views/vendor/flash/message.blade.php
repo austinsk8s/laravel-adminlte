c21698adda7924983c9af440eab78b971cdaf78f
Unknown <anthony.gilbert93@gmail.com>
2018-03-22 23:05:41 -0500

    permission change

--- resources/views/vendor/flash/message.blade.php
+++ resources/views/vendor/flash/message.blade.php
@@ -1,26 +1,20 @@
 @foreach ((array) session('flash_notification') as $message)
-    @if ($message['overlay'])
-        @include('flash::modal', [
-            'modalClass' => 'flash-modal',
-            'title'      => $message['title'],
-            'body'       => $message['message']
-        ])
-    @else
-        <div class="alert
-                    alert-{{ $message['level'] }}
-                    {{ $message['important'] ? 'alert-important' : '' }}"
-        >
-            @if ($message['important'])
-                <button type="button"
-                        class="close"
-                        data-dismiss="alert"
-                        aria-hidden="true"
-                >&times;</button>
-            @endif
+  @if ($message['overlay'] ?? false)
+    @include('flash::modal', [
+      'modalClass' => 'flash-modal',
+      'title'      => $message['title'],
+      'body'       => $message['message']
+    ])
+  @else
 
-            {!! $message['message'] !!}
-        </div>
-    @endif
+    @if ($message['important'] ?? false)
+      <div class="alert alert-{{ $message['level'] ?? '' }} {{ $message['important'] ? 'alert-important' : '' }}">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        {!! $message['message'] ?? '' !!}
+      </div>
+  @endif
+
+@endif
 @endforeach
 
 {{ session()->forget('flash_notification') }}
